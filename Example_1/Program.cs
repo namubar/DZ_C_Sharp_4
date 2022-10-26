9e00ee09b3c3223d15d864a40c49352aa54f64ab
namubar <mubarakshin.nail@tatar.ru>
2022-10-26 17:41:59 +0300

    Решил первые две задачи

---
+++ Example_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine ("Введите пятизначное число: ");
+int n = int.Parse (Console.ReadLine()!);
+int temp = n;
+int revDigit = 0;
+int isPalindrom (int n)
+{
+    //int temp = n;
+    //int revDigit = 0;
+    while (n>0)
+    {
+   //int revDigit = 0;
+    int digit = n % 10;
+    //int temp = n;
+    revDigit = revDigit * 10 + digit;
+    n = n / 10;
+    }
+    return revDigit;
+}    
+    isPalindrom (n);
+    
+    if (temp == revDigit)
+    {
+        Console.WriteLine ("Да, это палиндром");
+    }
+    else
+    {
+        Console.WriteLine ("Нет, это не палиндром");
+    }
+
---
+++ Example_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите координаты точки А (X,Y,Z)");
+int xa = int.Parse (Console.ReadLine()!);
+int ya = int.Parse (Console.ReadLine()!);
+int za = int.Parse (Console.ReadLine()!);
+Console.WriteLine ("Введите координаты точки B (X,Y,Z)");
+int xb = int.Parse (Console.ReadLine()!);
+int yb = int.Parse (Console.ReadLine()!);
+int zb = int.Parse (Console.ReadLine()!);
+
+double length (int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double result = 0;
+    result = Math.Sqrt (Math.Pow((xa-xb), 2)+ Math.Pow ((ya-yb), 2) +Math.Pow((za-zb), 2));
+    return result;
+}
+length (xa, ya, za, xb, yb, zb );
+//Console.WriteLine ("Расстояние равно " + length (xa, ya, za, xb, yb, zb ));
+Console.WriteLine (Math.Round (length (xa, ya, za, xb, yb, zb ), 2));
